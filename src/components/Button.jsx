d1a89990dcaf698515eeb6be5fdda384a39c9f24
igor-teixeira-souza <teixeiraigor525@gmail.com>
2025-11-18 11:27:16 -0300

    Hero section atualizada

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import { Mail } from "lucide-react";
+const NeuButton = () => {
+  return (
+    <div className="bg- min-h-[200px] flex items-center justify-center">
+      <button className="px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+        <a href="teixeiraigor525@gmail.com">
+          <Mail size={30} className="" />
+          Entre em contato!
+        </a>
+      </button>
+    </div>
+  );
+};
+
+export default NeuButton;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Mail, Linkedin, Github } from "lucide-react";
 import Particles from "../components/Particles";
 import TextType from "../components/TextType";
 
 const HeroSection = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <section className="min-h-screen bg-[#000000] flex items-center justify-center px-10 md:px-20 relative overflow-hidden">
-      <div style={{ width: "100%", height: "100%", position: "absolute" }}>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          zIndex: 0,
+        }}
+      >
         <Particles
           particleColors={["#ffffff", "#ffffff"]}
           particleCount={200}
@@ -18,7 +27,8 @@ const HeroSection = () => {
           disableRotation={false}
         />
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-between max-w-7xl w-full gap-10">
+
+      <div className="flex flex-col md:flex-row items-center justify-between max-w-7xl w-full gap-10 z-10">
         {/* Texto principal */}
         <div className="flex-1 space-y-6 text-center md:text-left">
           <p className="text-white text-lg">Olá, Meu nome é</p>
@@ -29,32 +39,48 @@ const HeroSection = () => {
             Frontend Developer
           </h2>
           <p className="text-amber-50 max-w-md">
-            Sou um desenvolvedor Frontend, focado na criação de experiências
-            web modernas e responsivas usando React, Tailwind CSS e princípios
-            de design limpo.
+            Sou um desenvolvedor Frontend, focado na criação de experiências web
+            modernas e responsivas usando React, Tailwind CSS e princípios de
+            design limpo.
           </p>
 
           {/* Botões de contato */}
           <div className="flex items-center gap-4 justify-center md:justify-start pt-4">
             <a
-              href="mailto:igor@email.com"
-              className="flex items-center gap-2 bg-blue-600 text-white px-5 py-3 rounded-full hover:bg-blue-700 transition duration-300 shadow-md"
+              href="mailto:teixeiraigor525@gmail.com"
+              className="flex items-center justify-center gap-2 bg-blue-600 text-white px-5 py-3 rounded-full hover:scale-105 transition-transform duration-100 min-w-[260px] "
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
             >
-              <Mail size={30} className=""/>
-              Entre em contato!
+              <Mail size={30} />
+              {isHovered ? (
+                <span className="font-medium text-sm md:text-base">
+                  teixeiraigor525@gmail.com
+                </span>
+              ) : (
+                <span className="font-medium">
+                 Contato
+                </span>
+              )}
             </a>
             <div className="flex gap-4">
               <a
                 href="#"
-                className="p-3 rounded-full border border-gray-300 hover:bg-blue-100 transition duration-300"
+                className="group p-3 rounded-full border border-gray-300 hover:bg-blue-100 hover:border-blue-100 transition duration-300"
               >
-                <Linkedin size={30} className="text-white" />
+                <Linkedin
+                  size={30}
+                  className="text-white group-hover:text-blue-600 transition-colors"
+                />
               </a>
               <a
                 href="#"
-                className="p-3 rounded-full border border-gray-300 hover:bg-blue-100 transition duration-300"
+                className="group p-3 rounded-full border border-gray-300 hover:bg-blue-100 hover:border-blue-100 transition duration-300"
               >
-                <Github size={30} className="text-white" />
+                <Github
+                  size={30}
+                  className="text-white group-hover:text-gray-900 transition-colors"
+                />
               </a>
             </div>
           </div>
@@ -69,9 +95,6 @@ const HeroSection = () => {
               className="w-full h-full object-cover scale-105 hover:scale-110 transition-transform duration-700"
             />
           </div>
-
-          {/* Elemento decorativo */}
-          <div className="absolute -z-10 w-[500px] h-[500px] bg-blue-100 rounded-full blur-3xl top-10 right-10 opacity-40"></div>
         </div>
       </div>
     </section>
